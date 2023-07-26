86093d4ffab5843ef0e67d21ecad090384a0ca10
KameiB <kameib.twitch@gmail.com>
2023-07-25 20:36:38 -0600

    mcmod.info adjustments

--- src/main/java/locloremixin/LocLoreMixin.java
+++ src/main/java/locloremixin/LocLoreMixin.java
@@ -7,7 +7,6 @@ import org.apache.logging.log4j.Logger;
 @Mod(modid = LocLoreMixin.MODID, version = LocLoreMixin.VERSION, name = LocLoreMixin.NAME)
 public class LocLoreMixin {
     public static final String MODID = "locloremixin";
-    public static final String VERSION = "1.0.0";
+    public static final String VERSION = "1.0.1";
     public static final String NAME = "LocLore Mixin";
-    public static final Logger LOGGER = LogManager.getLogger();
 }
